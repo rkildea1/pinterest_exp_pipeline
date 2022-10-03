3878d35d396b530695c6c546d09e6a4068d7005a
rkildea1 <31891933+rkildea1@users.noreply.github.com>
2022-10-03 13:38:08 +0100

    adding batch writer for consumer to s3

--- kafka/batch_consumer.py
+++ kafka/batch_consumer.py
@@ -1,5 +1,13 @@
 from kafka import KafkaConsumer
 from json import loads
+import boto3    
+from json import dumps
+import passwords
+
+s3client = boto3.client('s3', 
+                        aws_access_key_id=passwords.aws_access_key_id, 
+                        aws_secret_access_key=passwords.aws_secret_access_key
+                            )
 
 # create a consumer to retrieve the message from the topics
 data_stream_consumer = KafkaConsumer(
@@ -9,8 +17,12 @@ data_stream_consumer = KafkaConsumer(
 )
 # data_stream_consumer.subscribe(topics=["pinterest_streaming_uploads"])
 data_stream_consumer.subscribe(topics=["pinterestpipeline"])
+
+# dump each event dict in s3 as a json
 for message in data_stream_consumer:
-    print(message)
+    json_file = message.value
+    filename = (message.value['unique_id'])+'.json'
+    s3client.put_object(Body=dumps(json_file),Bucket='pinterestdata83436ecb',Key=filename)
+    print(f'...filename: {filename} was uploaded')
 
 
-    
