a13b0712979b008a04efbf18ba33c2857d114874
rkildea1 <31891933+rkildea1@users.noreply.github.com>
2022-10-23 16:31:56 +0100

    adding the create statement for postgres

--- cassandra_postgres_tables/postgres_create_table_script.sql
+++ cassandra_postgres_tables/postgres_create_table_script.sql
@@ -9,22 +9,9 @@ tag_list text,
 is_image_or_video text,
 image_src text,
 downloaded integer,
-save_location text
+save_location text,
 count_of_tags integer,
 timeStamp timestamp
 );
 
 
-
-schema = StructType() \
-    .add("category", StringType()) \
-    .add("index", IntegerType()) \
-    .add("unique_id", StringType()) \
-    .add("title", StringType()) \
-    .add("description", StringType()) \
-    .add("follower_count", StringType()) \
-    .add("tag_list", StringType()) \
-    .add("is_image_or_video", StringType()) \
-    .add("image_src", StringType()) \
-    .add("downloaded", IntegerType()) \
-    .add("save_location", StringType()) 
