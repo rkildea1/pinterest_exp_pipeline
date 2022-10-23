ed1d8c8ebce43dcac29138265bcf03380ccdb6b4
rkildea1 <31891933+rkildea1@users.noreply.github.com>
2022-10-23 16:12:30 +0100

    refactoring with function + timestamp

--- kafka/spark_streaming.py
+++ kafka/spark_streaming.py
@@ -2,14 +2,14 @@ import os
 import findspark
 findspark.init('/Users/ronan/spark-3.2.2-bin-hadoop3.2')
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, IntegerType, StringType
-from pyspark.sql.functions import from_json, col, size, split
+from pyspark.sql.types import StructType, IntegerType, StringType, TimestampType
+from pyspark.sql.functions import from_json, col, size, split, window
+from pyspark.sql.functions import current_timestamp
 
 
 
 # os.environ["PYSPARK_SUBMIT_ARGS"] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.2 spark_streaming.py pyspark-shell'
 os.environ["PYSPARK_SUBMIT_ARGS"] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.2 pyspark-shell'
-
 kafka_topic_name = 'pinterest_streaming_uploads' #topic to stream from
 # Kafka server to read data from.
 kafka_bootstrap_servers = 'localhost:9092'
@@ -33,7 +33,7 @@ schema = StructType() \
     .add("is_image_or_video", StringType()) \
     .add("image_src", StringType()) \
     .add("downloaded", IntegerType()) \
-    .add("save_location", StringType())
+    .add("save_location", StringType()) 
 
 
 # streaming DataFrame that reads from topic
@@ -47,10 +47,17 @@ stream_df = spark \
         .select(from_json(col("value").cast("string"), schema).alias("parsed_value")) \
         .select(col("parsed_value.*"))
 
-stream_df = stream_df.withColumn('count_of_tags', size(split(col("tag_list"), r",")))
+
+def transform_load(batch_df, batch_id):
+        batch_df = batch_df.withColumn('count_of_tags', size(split(col("tag_list"), r",")))
+        batch_df = batch_df.withColumn('timeStamp', current_timestamp())
+        batch_df.write.mode('append').format('console').save()
+
 
 stream_df.writeStream.outputMode("append") \
-    .format("console") \
-    .option("truncate", True) \
-    .start() \
-    .awaitTermination()
+        .foreachBatch(transform_load)\
+        .option("truncate", True) \
+        .start() \
+        .awaitTermination()
+
+
