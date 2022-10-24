48387a71c39e4f274236f192995224a830ed6bd0
rkildea1 <31891933+rkildea1@users.noreply.github.com>
2022-10-24 19:36:05 +0100

    adding feature to write datastream to postgres

--- kafka/spark_streaming.py
+++ kafka/spark_streaming.py
@@ -5,23 +5,22 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, IntegerType, StringType, TimestampType
 from pyspark.sql.functions import from_json, col, size, split, window
 from pyspark.sql.functions import current_timestamp
+import passwords
 
+os.environ["PYSPARK_SUBMIT_ARGS"] = '--packages  org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.2 --driver-class-path /Users/ronan/Desktop/GitHub/pinterest_exp_pipeline/jars/postgresql-42.5.0.jar --jars /Users/ronan/Desktop/GitHub/pinterest_exp_pipeline/jars/postgresql-42.5.0.jar pyspark-shell'
 
-
-# os.environ["PYSPARK_SUBMIT_ARGS"] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.2 spark_streaming.py pyspark-shell'
-os.environ["PYSPARK_SUBMIT_ARGS"] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.2 pyspark-shell'
 kafka_topic_name = 'pinterest_streaming_uploads' #topic to stream from
 # Kafka server to read data from.
 kafka_bootstrap_servers = 'localhost:9092'
 
-
+# create the session 
 spark = SparkSession \
         .builder \
         .appName("KafkaStreaming") \
         .getOrCreate()
 
 
-# define the schema
+# define the schema of each batch
 schema = StructType() \
     .add("category", StringType()) \
     .add("index", IntegerType()) \
@@ -36,7 +35,7 @@ schema = StructType() \
     .add("save_location", StringType()) 
 
 
-# streaming DataFrame that reads from topic
+#format of the source\ location\ 
 stream_df = spark \
         .readStream \
         .format("kafka") \
@@ -51,7 +50,15 @@ stream_df = spark \
 def transform_load(batch_df, batch_id):
         batch_df = batch_df.withColumn('count_of_tags', size(split(col("tag_list"), r",")))
         batch_df = batch_df.withColumn('timeStamp', current_timestamp())
-        batch_df.write.mode('append').format('console').save()
+        batch_df.write \
+        .mode('append') \
+        .format("jdbc") \
+        .option("url", "jdbc:postgresql://localhost/pinterest_streaming") \
+        .option("driver", "org.postgresql.Driver") \
+        .option("dbtable", 'pinterest_streaming_storage') \
+        .option("user", f'{passwords.postgresusername}') \
+        .option("password", f'{passwords.postgrespassword}') \
+        .save()
 
 
 stream_df.writeStream.outputMode("append") \
@@ -59,5 +66,6 @@ stream_df.writeStream.outputMode("append") \
         .option("truncate", True) \
         .start() \
         .awaitTermination()
+
 
 
